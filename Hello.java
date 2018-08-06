b112e21cec339e23a26b7e918d701d8b54d20130
Aron <1217924725@qq.com>
2018-08-06 18:03:31 +0800

    asda

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	private String mess;
+    public void setMess(String mess){
+        this.mess = mess;
+    }
+    public String getMess(){
+        return this.mess;
+    }
+}
---
+++ User.java
@@ -0,0 +1,8 @@
+public class User{
+	
+	public static void main(String[] args){
+	Hello h = new Hello();
+	h.setMess("Ni Hao!");
+	System.out.println(h.getMess());
+}	
+}
